a95b5db095a8be6bc1bfb4107273d465cc5c95f4
rakoc001 <chris_rakowski4@hotmail.com>
2025-09-01 12:30:37 +0100

    Moving files to sub folders

--- robofriends/src/containers/App.js
+++ robofriends/src/containers/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
-import CardList from './CardList';
-import SearchBox from './SearchBox';
-import Scroll from './Scroll';
+import CardList from '../components/CardList';
+import SearchBox from '../components/SearchBox';
+import Scroll from '../components/Scroll';
 import './App.css';
 
 class App extends React.Component {
@@ -23,14 +23,14 @@ class App extends React.Component {
         this.setState({ searchfield: event.target.value })
     }
 
-    render() {        
-        const filteredRobots = this.state.robots.filter(robot => {
-            return robot.name.toLowerCase().includes(this.state.searchfield.toLowerCase());
+    render() {
+        const {robots, searchfield } = this.state;  
+        const filteredRobots = robots.filter(robot => {
+            return robot.name.toLowerCase().includes(searchfield.toLowerCase());
         })
-        if (this.state.robots.length === 0) {
-            return <h1>Loading</h1>
-        } else {
-            return (
+        return !robots.length ?
+            <h1>Loading</h1> :
+            (
                 <div className='tc'>
                     <h1 className='f2'>RoboFriends</h1>
                     <SearchBox searchChange={this.onSearchChange}/>
@@ -38,8 +38,8 @@ class App extends React.Component {
                         <CardList robots={filteredRobots}/>
                     </Scroll>
                 </div>
-            )
-        }
+            );
+        
     }
 }
 
--- robofriends/src/index.js
+++ robofriends/src/index.js
@@ -1,7 +1,7 @@
 import React, { StrictMode, Fragment } from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+import App from './containers/App';
 import 'tachyons';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
